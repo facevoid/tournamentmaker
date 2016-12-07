bb1be59987c9afe58ba5a19b5904d1bc0cd9181a
saurav <sauravsust71@gmail.com>
2016-12-07 06:08:38 +0600

    first commit

---
+++ src/main/java/java/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package java.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by void on 12/2/16.
+ */
+@Controller
+public class HelloController {
+    @RequestM
+
+}
